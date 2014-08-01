50b3a0f7129f4a690eb43277a86ba8c2e226fc5a
Gavin Laking <gavinlaking@gmail.com>
2014-08-01 22:49:17 +0100

    Add placeholder test files.

---
+++ test/lib/playa/playlist_view.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'playa/playlist_view'
+
+module Playa
+  describe PlaylistView do
+
+  end
+end
---
+++ test/lib/playa/status_view_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'playa/status_view'
+
+module Playa
+  describe StatusView do
+
+  end
+end
