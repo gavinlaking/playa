97aa1549bd6636408e05d8d7e4c95f459f6b7645
Gavin Laking <gavinlaking@gmail.com>
2014-08-04 22:16:49 +0100

    Revert to simple status bar.

--- lib/playa/status_view.rb
+++ lib/playa/status_view.rb
@@ -2,30 +2,30 @@ require 'playa/view'
 
 module Playa
   class StatusView < View
-    def interface
-      'status'
-    end
+    # def interface
+    #   'status'
+    # end
 
-    def path
-      File.expand_path('../views/status.erb', __FILE__)
-    end
+    # def path
+    #   File.expand_path('../views/status.erb', __FILE__)
+    # end
 
     private
 
-    def output
-      self
-    end
-
     # def output
-    #   { 'status' => status }
+    #   self
     # end
+
+    def output
+      { 'status' => status }
+    end
 
     def type
-      :erb
+      :raw
     end
 
-    # def status
-    #   [ "\u{25B2} Prev  \u{25BC} Next  \u{21B2} Select  \u{2395} Pause   Q Quit" ]
-    # end
+    def status
+      [ "\u{25B2} Prev  \u{25BC} Next  \u{21B2} Select  \u{2395} Pause   Q Quit" ]
+    end
   end
 end
