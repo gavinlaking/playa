866f32ebab4a028ae651e04e417ea1fee11ccf6b
Gavin Laking <gavinlaking@gmail.com>
2014-09-07 18:09:23 +0100

    Refactor to use Vedeu's new Menu API;
    Also, add logger.

--- lib/playa/controllers/controller.rb
+++ lib/playa/controllers/controller.rb
@@ -2,98 +2,92 @@ module Playa
   class Controller
     include Vedeu
 
-    def initialize(args = [])
-      @player = Player.new
+    event :complete do
+      trigger(:_menu_next_, 'playlist')
+      trigger(:_menu_select_, 'playlist')
+      trigger(:select, trigger(:_menu_selected_, 'playlist'))
+      trigger(:update)
+    end
 
-      event :_initialize_ do
-        trigger(:show_startup)
+    event :_initialize_ do
+      trigger(:show_startup)
+    end
+
+    event :key do |key|
+      case key
+      when :left, 'h'   then trigger(:rewind)
+      when :right, 'l'  then trigger(:forward)
+      when ' '          then trigger(:toggle)
+      when :up, 'k'     then
+        trigger(:_menu_prev_, 'playlist')
+        trigger(:update)
+
+      when :down, 'j'   then
+        trigger(:_menu_next_, 'playlist')
+        trigger(:update)
+
+      when '?'          then trigger(:show_help)
+      when 'p', 's'     then trigger(:show_player)
+      when 'q'          then trigger(:_exit_)
+      when :enter       then
+        trigger(:_menu_select_, 'playlist')
+        trigger(:select, trigger(:_menu_selected_, 'playlist'))
+        trigger(:update)
+
       end
+    end
+
+    event :select do |track|
+      trigger(:play, track)
+    end
+
+    event :update do
+      PlaylistView.render
+      trigger(:_refresh_playlist_)
+    end
+
+    def initialize(args = [])
+      @args   = args
+
+      @player = Player.new
+      @player.events.on(:position_change) { trigger(:progress_update) }
+      @player.events.on(:complete)        { trigger(:complete) }
 
       event :show_startup do
         trigger(:_clear_)
-
         StartupView.render
-
         trigger(:_refresh_group_player_)
       end
 
       event :show_help do
         trigger(:_clear_)
-
         HelpView.render
-
         trigger(:_refresh_help_)
       end
 
       event :show_player do
         trigger(:_clear_)
-
-        PlaylistView.render(menu)
+        PlaylistView.render
         StatusView.render
         ProgressView.render(@player)
-
         trigger(:_refresh_group_player_)
       end
 
-      event :update do
-        PlaylistView.render(menu)
-        trigger(:_refresh_playlist_)
-      end
-
-      event :progress_update, 0.5 do
+      event(:progress_update, { delay: 0.5 }) do
         ProgressView.render(@player)
         trigger(:_refresh_progress_)
       end
 
-      event :select do |track|
-        trigger(:play, track)
-      end
-
-      event :complete do
-        trigger(:menu_next)
-        trigger(:menu_select)
-        trigger(:select, menu.current_item)
-        trigger(:update)
-      end
-
-      event :key do |key|
-        case key
-        when :left, 'h'  then trigger(:rewind)
-        when :right, 'l' then trigger(:forward)
-        when ' '    then trigger(:toggle)
-        when :up, 'k' then
-          trigger(:menu_prev)
-          trigger(:update)
-        when :down, 'j' then
-          trigger(:menu_next)
-          trigger(:update)
-        when '?' then trigger(:show_help)
-        when 'p', 's' then trigger(:show_player)
-        when 'q' then trigger(:_exit_)
-        when :enter
-          trigger(:menu_select)
-          trigger(:select, menu.current_item)
-          trigger(:update)
-        end
-      end
-
-      @player.events.on(:position_change) { trigger(:progress_update) }
-      @player.events.on(:complete)        { trigger(:complete) }
-
-      @args = args
-      trigger(:show_startup)
+      menu('playlist') { items(tracks) }
     end
 
     private
 
     attr_reader :args
 
-    def menu
-      @_menu ||= Vedeu::Menu.new(tracks)
+    def tracks
+      @tracks ||= TrackCollection.new(args).tracks
     end
 
-    def tracks
-      @_tracks ||= TrackCollection.new(args).tracks
-    end
   end
 end
